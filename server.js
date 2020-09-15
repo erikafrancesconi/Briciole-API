41452ff16d35f2b013ba7e87b4d5b6af3638a7f6
Erika Francesconi <fiorediren@gmail.com>
2020-09-15 17:29:27 +0200

    First connection to Postgres

--- server.js
+++ server.js
@@ -1,33 +1,49 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
+const { Client } = require('pg');
 
 const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
-let lastPwd = ''; // TODO: remove this shit
+const client = new Client({
+  user: 'postgres',
+  host: '127.0.0.1',
+  database: 'briciole',
+  password: 'postgres',
+  port: 5432,
+})
+client.connect()
 
 app.post('/register', (req, res) => {
+  // TODO: add a transaction
   const { name, email, password} = req.body;
-  console.log(name);
-  console.log(email);
-  console.log(password);
 
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(password, salt, (err, hash) => {
-        // Store hash in your password DB.
-        lastPwd = hash;
-        console.log(lastPwd);
+  let text = 'INSERT INTO users (fullname, email, joined) VALUES($1, $2, $3)';
+  let values = [name, email, new Date()];
+
+  client.query(text, values)
+  .then(() => {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, (err, hash) => {
+        text = 'INSERT INTO login (email, hash) VALUES($1, $2)';
+        values = [email, hash];
+
+        client.query(text, values)
+        .then(() => {
+          res.json({result: 'OK'});
+        })
+        .catch(() => {
+          res.json({result: 'Unable to register.'});
+        })
+      });
     });
-  });
-
-  if (false) {
-    res.status(400).json('Something went wrong');
-  }
-
-  res.json({result: 'OK', id: 123, name: 'Erika'});
+  })
+  .catch(() => {
+    res.json({result: 'Unable to register.'});
+  })
 });
 
 app.post('/signin', (req, res) => {
