9e66523dc07eb6611e514d5929b1f137dc282db1
Erika Francesconi <fiorediren@gmail.com>
2020-09-16 09:28:12 +0200

    Better connection management? + Login

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
-const { Client } = require('pg');
+const { Pool, Client } = require('pg');
 
 const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
-const client = new Client({
+const pool = new Pool({
   user: 'postgres',
   host: '127.0.0.1',
   database: 'briciole',
   password: 'postgres',
   port: 5432,
+});
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
 })
-client.connect()
 
 app.post('/register', (req, res) => {
   // TODO: add a transaction
@@ -24,54 +27,84 @@ app.post('/register', (req, res) => {
   let text = 'INSERT INTO users (fullname, email, joined) VALUES($1, $2, $3)';
   let values = [name, email, new Date()];
 
-  client.query(text, values)
-  .then(() => {
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, (err, hash) => {
-        text = 'INSERT INTO login (email, hash) VALUES($1, $2)';
-        values = [email, hash];
+  pool.connect()
+  .then(client => {
+    const abort = err => {
+      if (err) {
+        console.error('Error in transaction', err.stack);
+        client.query('ROLLBACK', err => {
+          if (err) {
+            console.error('Error rolling back client', err.stack);
+          }
+        })
+      }
+    }
 
-        client.query(text, values)
-        .then(() => {
-          res.json({result: 'OK'});
-        })
-        .catch(() => {
-          res.json({result: 'Unable to register.'});
-        })
-      });
-    });
+    client.query('BEGIN')
+    .then(() => {
+      client.query(text, values)
+      .then(() => {
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(password, salt, (err, hash) => {
+            text = 'INSERT INTO login (email, hash) VALUES($1, $2)';
+            values = [email, hash];
+    
+            client.query(text, values)
+            .then(() => {
+              client.query('COMMIT')
+              .then(() => {
+                res.json({result: 'OK'});
+              })
+              .catch(err => {
+                console.error('Error committing transaction', err.stack);
+                res.json({result: 'Unable to register.'});
+              });
+            })
+            .catch(err => {
+              abort(err)
+              res.json({result: 'Unable to register.'});
+            })
+          });
+        });
+      })
+      .catch((err) => {
+        abort(err);
+        res.json({result: 'Unable to register.'});
+      })
+    })
   })
-  .catch(() => {
+  .catch(err => {
+    console.log('Unable to connect', err.stack);
     res.json({result: 'Unable to register.'});
   })
 });
 
 app.post('/signin', (req, res) => {
   const { email, password } = req.body;
-  console.log(email);
-  console.log(password);
-
-  bcrypt.compare(password, lastPwd, (err, res) => {
-    if (res) {
-      console.log('Password matches');
+  
+  let text = 'SELECT U.id, fullname, hash FROM users U INNER JOIN login L ON U.email=L.email WHERE U.email = $1';
+  pool.query(text, [email])
+  .then(resp => {
+    if (resp.rowCount === 1) {
+      const { hash, id, fullname } = resp.rows[0];
+      bcrypt.compare(password, hash, (err, resq) => {
+        if (resq) {
+          res.json({result: 'OK', id: id, name: fullname});
+        }
+        else {
+          res.json({result: 'Wrong Credentials'});
+        }
+      });
+    } else {
+      res.json({result: 'Wrong Credentials'});
     }
-    else {
-      console.log('Wrong password');
-    }
-  });
-
-  if (false) {
-    res.status(400).json({result: 'Wrong Credentials'});
-  }
-
-  res.json({result: 'OK', id: 123, name: 'Erika'});
+  })
+  .catch(() => {
+    res.json({result: 'Wrong Credentials'});
+  })
 });
 
 app.post('/signout', (req, res) => {
-  if (false) {
-    res.status(400).json('Something went wrong');
-  }
-
   res.json('OK');
 });
 
