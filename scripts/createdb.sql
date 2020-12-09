ec1c50fbc094a7819ccd41967d3cd36d463caab6
Erika Francesconi <fiorediren@gmail.com>
2020-12-09 14:50:51 +0100

    Updating SQL scripts

--- scripts/createdb.sql
+++ scripts/createdb.sql
@@ -1,5 +1,4 @@
 -- DROP TABLE users;
-
 CREATE TABLE users (
 	id serial NOT NULL,
 	fullname varchar(100) NOT NULL,
@@ -10,32 +9,20 @@ CREATE TABLE users (
 	CONSTRAINT users_pk PRIMARY KEY (id)
 );
 
--- DROP TABLE bookmarks;
-
-CREATE TABLE bookmarks (
-	id serial NOT NULL,
-	userid int4 NOT NULL,
-	url text NOT NULL,
-	CONSTRAINT bookmarks_pk PRIMARY KEY (id),
-	CONSTRAINT bookmarks_fk FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE
-);
-
 -- DROP TABLE login;
-
 CREATE TABLE login (
 	id serial NOT NULL,
 	hash varchar(100) NOT NULL,
 	email text NOT NULL,
 	lastlogin timestamp NULL,
 	CONSTRAINT login_email_un UNIQUE (email),
-	CONSTRAINT login_pk PRIMARY KEY (id),
-	CONSTRAINT login_fk FOREIGN KEY (email) REFERENCES users(email) ON DELETE CASCADE
+	CONSTRAINT login_pk PRIMARY KEY (id)
 );
+ALTER TABLE login ADD CONSTRAINT login_fk FOREIGN KEY (email) REFERENCES users(email) ON DELETE CASCADE;
 
 -- DROP TABLE user_verification;
-
 CREATE TABLE user_verification (
 	userid int4 NOT NULL,
-	hash varchar(40) NOT NULL,
-	CONSTRAINT user_verification_fk FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE
+	hash varchar(40) NOT NULL
 );
+ALTER TABLE user_verification ADD CONSTRAINT user_verification_fk FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE;
